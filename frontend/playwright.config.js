7310a2347955957817d0a1ff3921847f1398d825
Pasha Dnipro <pzadniprovskij@gmail.com>
2026-06-25 01:44:32 +0300

    test(report): add Allure report publishing

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e/tests',
+  
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['allure-playwright', { outputFolder: 'allure-results' }],
+  ],
+
   use: {
     baseURL: 'http://localhost:5173',
     headless: true,
